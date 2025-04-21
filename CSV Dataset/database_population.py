0c1dcde73f0a51e475c9467165197be816fde3ee
Peter Yeung <pjyeung03@gmail.com>
2025-04-21 11:46:04 -0400

    Completely Populated Database

--- CSV Dataset/database_population.py
+++ CSV Dataset/database_population.py
@@ -17,15 +17,12 @@ cursor.execute("CREATE TABLE IF NOT EXISTS Users (email TEXT, password TEXT, PRI
 cursor.execute("CREATE TABLE IF NOT EXISTS Helpdesk (email TEXT, position TEXT, PRIMARY KEY (email))")
 cursor.execute("CREATE TABLE IF NOT EXISTS Buyers (email TEXT, business_name TEXT, buyer_address_id TEXT, PRIMARY KEY (email))")
 cursor.execute("CREATE TABLE IF NOT EXISTS Sellers (email TEXT, business_name TEXT, business_address_id TEXT, bank_routing_number TEXT, bank_account_number INTEGER, balance INTEGER, PRIMARY KEY (email))")
-
 cursor.execute("CREATE TABLE IF NOT EXISTS Address (address_id TEXT, zipcode INTEGER, street_num INTEGER, street_name TEXT, PRIMARY KEY (address_id))")
 cursor.execute("CREATE TABLE IF NOT EXISTS Credit_Cards (credit_card_num TEXT, card_type TEXT, expire_month INTEGER, expire_year INTEGER, security_code INTEGER, owner_email TEXT, PRIMARY KEY (credit_card_num))")
 cursor.execute("CREATE TABLE IF NOT EXISTS Zipcode_Info (zipcode INTEGER, city TEXT, state TEXT, PRIMARY KEY (zipcode))")
-
 cursor.execute("CREATE TABLE IF NOT EXISTS Product_Listings (seller_email TEXT, listing_id INTEGER, category  TEXT, product_title TEXT, product_name TEXT, product_description TEXT, quantity INTEGER, product_price TEXT, status INTEGER, PRIMARY KEY (seller_email, listing_id))")
 cursor.execute("CREATE TABLE IF NOT EXISTS Orders (order_id INTEGER, seller_email TEXT, listing_id INTEGER, buyer_email TEXT, date TEXT, quantity INTEGER, payment INTEGER, PRIMARY KEY (order_id))")
-cursor.execute("CREATE TABLE IF NOT EXISTS Categories (parent_category TEXT, category_name TEXT, PRIMARY KEY (parent_category))")
-
+cursor.execute("CREATE TABLE IF NOT EXISTS Categories (parent_category TEXT, category_name TEXT)")
 cursor.execute("CREATE TABLE IF NOT EXISTS Requests (request_id INTEGER, sender_email TEXT, helpdesk_staff_email TEXT, request_type TEXT, request_desc TEXT, request_status INTEGER, PRIMARY KEY (request_id))")
 cursor.execute("CREATE TABLE IF NOT EXISTS Reviews (order_id INTEGER, rate INTEGER, review_desc TEXT, PRIMARY KEY (order_id))")
 
@@ -58,7 +55,6 @@ with open("Sellers.csv", "r", encoding="utf-8") as file:
     for row in reader:
         email, business_name, business_address_id, bank_routing_number, bank_account_number, balance = row
         cursor.execute("INSERT INTO Sellers (email, business_name, business_address_id, bank_routing_number, bank_account_number,balance) VALUES (?, ?, ?, ?, ?, ?)", (email, business_name, business_address_id, bank_routing_number, bank_account_number,balance))
-
 with open("Address.csv", "r", encoding="utf-8") as file:
     reader = csv.reader(file)
     next(reader)
@@ -80,6 +76,41 @@ with open("Zipcode_Info.csv", "r", encoding="utf-8") as file:
     for row in reader:
         zipcode, city, state = row
         cursor.execute("INSERT INTO Zipcode_Info (zipcode, city, state) VALUES (?, ?, ?)", (zipcode, city, state))
+with open("Product_Listings.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        seller_email, listing_id, category, product_title, product_name, product_description, quantity, product_price, status = row
+        cursor.execute("INSERT INTO Product_Listings (seller_email, listing_id, category, product_title, product_name, product_description, quantity, product_price, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (seller_email, listing_id, category, product_title, product_name, product_description, quantity, product_price, status))
+with open("Orders.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        order_id, seller_email, listing_id, buyer_email, date, quantity, payment = row
+        cursor.execute("INSERT INTO Orders (order_id, seller_email, listing_id, buyer_email, date, quantity, payment) VALUES (?, ?, ?, ?, ?, ?, ?)", (order_id, seller_email, listing_id, buyer_email, date, quantity, payment))
+with open("Categories.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        parent_category, category_name = row
+        cursor.execute("INSERT INTO Categories (parent_category, category_name) VALUES (?, ?)", (parent_category, category_name))
+with open("Requests.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        request_id, sender_email, helpdesk_staff_email, request_type, request_desc, request_status = row
+        cursor.execute("INSERT INTO Requests (request_id, sender_email, helpdesk_staff_email, request_type, request_desc, request_status) VALUES (?, ?, ?, ?, ?, ?)", (request_id, sender_email, helpdesk_staff_email, request_type, request_desc, request_status))
+with open("Reviews.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        order_id, rate, review_desc = row
+        cursor.execute("INSERT INTO Reviews (order_id, rate, review_desc) VALUES (?, ?, ?)", (order_id, rate, review_desc))
 
 conn.commit()
 conn.close()
